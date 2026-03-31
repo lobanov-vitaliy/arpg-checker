f862d1dba0ebed10b8a1546d88f761121fd636a9
vilat <vilat@saldoapps.com>
2026-03-31 16:32:19 +0300

    add new games

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { Analytics } from "@vercel/analytics/next";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <>
+      <Analytics />
+      {children}
+    </>
+  );
 }
